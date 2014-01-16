7adf2ff040d0fc73e46e59a7d775640ad60784f4
darrell <darrell.camsoun>
2014-01-16 10:28:36 -0800

    Start of a Motor class for the airline industry.

---
+++ motors/src/motors/Motor.java
@@ -0,0 +1,34 @@
+package motors;
+
+public class Motor {
+	private String serialNumber;
+	private int hoursOfService;
+	private int maximumHoursBeforeService;
+
+	public Motor(String inSerialNumber, int inMaximumHoursBeforeService) {
+		serialNumber = inSerialNumber;
+		hoursOfService = 0;
+		maximumHoursBeforeService = inMaximumHoursBeforeService;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public int getHoursOfService() {
+		return hoursOfService;
+	}
+
+	public int getMaximumHoursBeforeService() {
+		return maximumHoursBeforeService;
+	}
+
+	public void addHoursOfService(int moreFlightHours) {
+		hoursOfService += moreFlightHours;
+	}
+
+	public int hoursOfServiceLeft() {
+		int hoursLeft = maximumHoursBeforeService - hoursOfService;
+		return hoursLeft;
+	}
+}
