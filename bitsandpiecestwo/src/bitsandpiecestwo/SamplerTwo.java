adb9423db1e249c092a2baa90af0fae40f795df0
darrell <darrell.camsoun>
2014-02-25 10:23:36 -0800

    Added more mostly floating point examples

--- bitsandpiecestwo/src/bitsandpiecestwo/SamplerTwo.java
+++ bitsandpiecestwo/src/bitsandpiecestwo/SamplerTwo.java
@@ -1,4 +1,5 @@
 package bitsandpiecestwo;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +39,58 @@ public class SamplerTwo {
 		
 		int valueSomething = first;
 		// Integer valueSomething = first.intValue();
+		
+		float result = 2.0F + (6/7.0F);
+		
+		System.out.println(result);
+		
+		int aVar;
+		long bVar;
+		
+		float cVar = 10.0F;
+		double dVar = 10.0;
+		
+		System.out.println(10/3.0);
+		
+		double firstDouble = 0.66666/0.43333;
+		double secondDouble = 66666.0/43333.0;
+		
+		if(firstDouble == secondDouble) {
+			System.out.println("Doubles are the same");
+		}
+		
+		System.out.println(firstDouble + " " + secondDouble);
+		
+		double precision = 1.0E-06;
+		System.out.println(precision);
+		
+		DecimalFormat formatter = new DecimalFormat("0.0000000");
+		System.out.println("Precision " + formatter.format(precision));
+	
+		System.out.format("****  %4.2f has some value   %4.2f\n", firstDouble, secondDouble);
+		
+		double difference = firstDouble - secondDouble;
+		
+		// which is bigger or smaller???
+		
+		Double firstBigDouble = firstDouble;
+		Double secondBigDouble = secondDouble;
+		
+		if(firstBigDouble.compareTo(secondBigDouble) > 0) {
+			System.out.println("First is bigger than second");
+		}
+	}
+	
+	public boolean withinDelta(double first, double second, double plusMinus) {
+		if(first + plusMinus > second) {
+			return false;
+		}
+			
+		if(first - plusMinus < second) {
+			return false;
+		}
+		
+		return true;
 	}
 
 }
